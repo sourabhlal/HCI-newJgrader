0774fd348c895cfc9a4b9b8cd899bf02cab948aa
sourabhlal <sourabhlal@yahoo.co.in>
2015-03-22 18:55:50 +0100

    work done so far on solutions
    also updated readme

---
+++ js/solutions.js
@@ -0,0 +1,12 @@
+$(".assignmentOption").each(function() {
+    $(this).hover(
+    	function() {
+    		$( this ).css( "background-color", "#4595D1" );
+  		}, function() {
+    		$( this ).css( "background-color", "#EFEFF0" );
+  		}
+    );
+});
+
+
+
