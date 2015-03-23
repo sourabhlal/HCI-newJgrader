bb4fab216887186742bc047c5490a9f66660aabe
sourabhlal <sourabhlal@yahoo.co.in>
2015-03-23 03:48:59 +0100

    Super cool jquery shizz

--- js/solutions.js
+++ js/solutions.js
@@ -1,13 +1,61 @@
-$(".assignmentOption").each(function() {
-    $(this).hover(
-    	function() {
-    		$( this ).css( "background-color", "#4595D1" );
-  		}, function() {
-    		$( this ).css( "background-color", "#EFEFF0" );
-  		}
-    );
+$( document ).ready(function() {
+	console.log("bla");
+    $( "#work" ).css( "background-color", "#808080" );
+    $( "#work" ).addClass( "selectedOption" );
 });
 
-$('#solution').on('click', '.myselector', function(e) {
-    alert('Parameter: ' + $(this).attr('data-param'));
+$('#work').on('click', function() {
+		$( ".selectedOption" ).addClass( "assignmentOption" );
+		$( ".selectedOption" ).css( "background-color", "#EFEFF0" );
+		$( ".selectedOption" ).removeClass( "selectedOption" );
+		$( "#work" ).addClass( "selectedOption" );
+	    $( "#work" ).removeClass( "assignmentOption" );
+	    $( "#work" ).css( "background-color", "#808080" );
+    	$("#itemToDisplay").html("<object id=\"pdf\" width=\"100%\" type=\"application/pdf\" data=\"docs/SubmittedAndGradedCC2.pdf\" id=\"pdf_content\"><p>.</p></object>");
 });
+
+$('#solution').on('click', function() {
+		$( ".selectedOption" ).addClass( "assignmentOption" );
+		$( ".selectedOption" ).css( "background-color", "#EFEFF0" );
+		$( ".selectedOption" ).removeClass( "selectedOption" );
+		$( "#solution" ).addClass( "selectedOption" );
+	    $( "#solution" ).removeClass( "assignmentOption" );
+	    $( "#solution" ).css( "background-color", "#808080" );
+    	$("#itemToDisplay").html("<object id=\"pdf\" width=\"100%\" type=\"application/pdf\" data=\"docs/sol1.pdf\" id=\"pdf_content\"><p>File not available. Please contact your TA or Professor for more info.</p></object>");
+});
+
+$('#grade').on('click', function() {
+		$( ".selectedOption" ).addClass( "assignmentOption" );
+		$( ".selectedOption" ).css( "background-color", "#EFEFF0" );
+		$( ".selectedOption" ).removeClass( "selectedOption" );
+		$( "#grade" ).addClass( "selectedOption" );
+	    $( "#grade" ).removeClass( "assignmentOption" );
+	    $( "#grade" ).css( "background-color", "#808080" );
+    	$("#itemToDisplay").html("grade");
+});
+
+$('#message').on('click', function() {
+		$( ".selectedOption" ).addClass( "assignmentOption" );
+		$( ".selectedOption" ).css( "background-color", "#EFEFF0" );
+		$( ".selectedOption" ).removeClass( "selectedOption" );
+		$( "#message" ).addClass( "selectedOption" );
+	    $( "#message" ).removeClass( "assignmentOption" );
+	    $( "#message" ).css( "background-color", "#808080" );
+    	$("#itemToDisplay").html("message");
+});
+
+
+$(".assignmentOption").each(function() {
+    $(this).mouseover(function() {
+    	$( this ).css( "background-color", "#4595D1" );
+  	}).mouseout(function() {
+  		if ($(this).hasClass("selectedOption")){
+    		$( this ).css( "background-color", "#808080" );
+		};
+		if (!$(this).hasClass("selectedOption")){
+    		$( this ).css( "background-color", "#EFEFF0" );
+    	};
+  	});
+});
+
+
