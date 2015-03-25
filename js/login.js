648bb0280fc3a71b2c5282391c3be3ad9ff378a1
Mihai Fieraru <fierarufmihai@gmail.com>
2015-03-25 23:19:02 +0100

    select courses

--- js/login.js
+++ js/login.js
@@ -3,13 +3,21 @@ $(function(){
 	var email = $("#emailadd").val(); 
 	var pass = $("#Password").val(); 
 
-	if(email == "test@user.com"){
+	if(email == "old@user.com"){
 		if(pass == "1234"){
 			location.href="./homepage.html"; 
 		} else {
 			alert("Invalid Password"); 
 		}
-	} else {
+	} 
+	else if(email == "new@user.com"){
+		if(pass == "1234"){
+			location.href="./select-courses.html"; 
+		} else {
+			alert("Invalid Password"); 
+		}
+	} 
+	else {
 		alert("Invalid UserID"); 
 	}
 	return false; 
